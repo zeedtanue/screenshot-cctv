e66d62e5e4511cf76c68389c9722351a4fc0f14c
zeedtanue <zeedtanue@gmail.com>
2021-02-03 16:52:20 +0800

    updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const puppeteer= require('puppeteer');
 const path= require('path');
+const { throws, rejects } = require('assert');
+const { setInterval } = require('timers');
 
 
 
@@ -9,26 +11,31 @@ let browserPromise = puppeteer.launch({product:"chrome",
     ]
 });
 
-const screenshot = async(req,res)=>{
-    
+const screenshot = async()=>{
+    try {
+        
     const url =  'https://www.youtube.com/watch?v=FZ02VT9XACY';
     const browser = await browserPromise;
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage()
 
     await page.goto(url);
-    const ss =setInterval( async()=>{
-        const date= new Date()
-        const timeString = `${date.toDateString()} Time${date.getHours()}-${date.getMinutes()}-${ date.getUTCMilliseconds()}`
     
-        await page.screenshot({
-         type:'png',
-        path: path.join(__dirname,`./public/live/screenshot${timeString.toString()}.png`),
+    const date= new Date()
+    const timeString = `${date.toDateString()} Time${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}-${ date.getUTCMilliseconds()}`
     
+    await page.screenshot({
+      type:'png',
+      path: path.join(__dirname,`./public/live/screenshot${timeString.toString()}.png`),
     });
-    console.log(`screenshot has been saved screenshot${timeString.toString()}.png`)},3*60*100)
+    console.log(`screenshot has been saved screenshot${timeString.toString()}.png`)
 
-    context.close()
+    await context.close()
+        
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 
-screenshot()
+setInterval(screenshot,30000)
