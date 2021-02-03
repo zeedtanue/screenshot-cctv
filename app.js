5ffeb9cc226d2a1d4eeaecba6109f5486cf250cf
zeedtanue <zeedtanue@gmail.com>
2021-02-03 15:41:48 +0800

    initial

---
+++ app.js
@@ -0,0 +1,35 @@
+const puppeteer= require('puppeteer');
+const path= require('path');
+
+
+
+let browserPromise = puppeteer.launch({product:"chrome",
+    args:[
+        '--no-sandbox',
+    ]
+});
+
+const screenshot = async(req,res)=>{
+    
+    const url =  'https://www.youtube.com/watch?v=FZ02VT9XACY';
+    const browser = await browserPromise;
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage()
+
+    await page.goto(url);
+    const ss =setInterval( async()=>{
+        const date= new Date()
+        const timeString = `${date.toDateString()} Time${date.getHours()}-${date.getMinutes()}-${ date.getUTCMilliseconds()}`
+    
+        await page.screenshot({
+         type:'png',
+        path: path.join(__dirname,`./public/ss/screenshot${timeString.toString()}.png`),
+    
+    });
+    console.log(`screenshot has been saved screenshot${timeString.toString()}.png`)},5000)
+
+    setTimeout(ss, 1600000)
+    context.close()
+}
+
+screenshot()
